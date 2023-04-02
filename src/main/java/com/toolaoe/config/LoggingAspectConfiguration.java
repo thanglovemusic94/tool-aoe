05cb66d93b15fc21f5f3cac4a461ef6603f76ba9
thanglovemusic94 <thanglovemusic94@gmail.com>
2023-04-02 22:08:40 +0700

    Initial version of toolaoe generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/toolaoe/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.toolaoe.config;
+
+import com.toolaoe.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
